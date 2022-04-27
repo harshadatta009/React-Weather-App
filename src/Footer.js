f5ee2a367521a30816a52df6978bc542260f224c
harshadatta009 <harsha_datta@yahoo.com>
2022-04-28 00:59:43 +0530

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className='d-flex align-items-center justify-content-center'>
+        Done by harsha datta
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
